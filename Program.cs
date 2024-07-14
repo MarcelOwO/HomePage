080e851ddab81002b2698c96a296f80dcf481379
Marcel Sigl <marcel-sigl@gmx.de>
2024-07-15 01:54:03 +0200

    changes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HomePage;
+using HomePage.wwwroot.Services;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
+
+builder.Services.AddTransient<MudLocalizer, DictionaryMudLocalizer>();
+
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
 await builder.Build().RunAsync();
