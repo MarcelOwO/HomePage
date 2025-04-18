15b7385bbe47548f603855240e57a7b2e2d33eab
MarcelOwO <marcel-sigl@gmx.de>
2025-04-18 19:37:08 +0200

    adding file to git

---
+++ Models/NavMenuPageData.cs
@@ -0,0 +1,8 @@
+﻿namespace HomePage.Models;
+
+public class NavMenuPageData
+{
+    public required string Name { get; set; }
+    public required string Link { get; set; }
+    public required string Icon { get; set; }
+}
